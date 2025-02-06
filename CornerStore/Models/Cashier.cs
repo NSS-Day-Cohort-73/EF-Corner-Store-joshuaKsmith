5aff14f3d7fb68ad9f0b95db297d914529d39206
Joshua Smith <mtgmirage@protonmail.com>
2025-02-06 11:14:36 -0600

    created initial class definitions and ERD.dbml file

--- CornerStore/Models/Cashier.cs
+++ CornerStore/Models/Cashier.cs
@@ -1,6 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CornerStore.Models;
+
 
 public class Cashier
 {
+    public int Id { get; set; }
+    [Required]
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string FullName => FirstName + LastName;
+    public List<Order> Orders { get; set; }
 
 }
--- CornerStore/Models/Order.cs
+++ CornerStore/Models/Order.cs
@@ -1,7 +1,14 @@
+using System.ComponentModel.DataAnnotations;
 
 namespace CornerStore.Models;
 
 public class Order
 {
-
+    public int Id { get; set; }
+    [Required]
+    public int CashierId { get; set; }
+    public Cashier Cashier { get; set; }
+    public decimal Total => OrderProducts.Sum(op => op.Product.Price * op.Quantity);
+    public DateTime? PaidOnDate { get; set; }
+    public List<OrderProduct> OrderProducts { get; set; }
 }
